4e8b8daf7288f6c5deb313b56b173da3847d9117
Kamol-Paul <s1910176121@ru.ac.bd>
2022-11-15 23:06:27 +0600

    be more perfect

--- dft-idft-and-its-shit-properties/time_shift.m
+++ dft-idft-and-its-shit-properties/time_shift.m
@@ -3,15 +3,22 @@ x = [1 1 2 2 3 3 4 4];
 
 X = MyDft(x);
 xx = MyIDft(X);
-subplot(221);
+subplot(421);
 stem(n,x);
 axis([0 7 0 4]);
-subplot(222);
+subplot(422);
 stem(n,abs(X));
-subplot(223);
+subplot(423);
 stem(n,xx);
 
 m = -2;
 XX =  MyTimeShift(X,m);
-subplot(224);
-stem(n,MyIDft(XX));
+%subplot(424);
+%stem(n,MyIDft(XX));
+
+for l = 1:5
+    Ax = MyTimeShift(X,l);
+    subplot(4,2,l+3);
+    stem(n,MyIDft(Ax));
+    title(strcat("for the time shift of " + string(l)));
+end
